57a40a77a47dc28f1c4e37b64081e9e7bb9e4255
vagrant <vagrant@localhost.localdomain>
2018-07-24 06:44:17 +0000

    initial workstation cookbook

---
+++ recipes/setup.rb
@@ -0,0 +1,18 @@
+package 'tree' do
+  action :install
+end
+
+package 'ntp' do
+  action :install
+end
+
+package 'git' do
+  action :install
+end
+
+file '/tmp/motd.txt' do
+  content 'This server is the propert of Altaf'
+  owner 'root'
+  group 'root'
+  action :create
+end
